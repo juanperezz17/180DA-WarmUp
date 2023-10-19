996b4cec77a9fe5dd0f0339d74292d0fe435ec02
juanperezz17 <juanperez13@g.ucla.edu>
2023-10-19 02:23:18 +0100

    First push from Pi

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 if __name__ == '__main__':
 	x = input(f"Which course are you taking?")
 	if x == "ECE 180":
-		print(x + " is the best class ever")
+		print(x + " is the best class ever\n")
 	else:
 		print("Bruins are the best!")
+
