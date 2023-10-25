03f19d0269d5487bb85eed18f74a346a07701ad1
juanperezz17 <juanperez13@g.ucla.edu>
2023-10-25 01:57:42 +0100

    Pushed from Pi

--- lab3/clientTest.py
+++ lab3/clientTest.py
@@ -1,7 +1,7 @@
 import socket
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('192.168.1.246', 8080))
-client.sendall('I am CLIENT\n'.encode())
+client.sendall('I am CLIENT'.encode())
 from_server = client.recv(4096)
 client.close()
 print(from_server)
